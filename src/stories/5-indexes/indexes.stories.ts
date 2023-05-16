bfb47d6d24c158b771f0e69f3b960ff1dea987eb
Josh Mabry <mabry1985@gmail.com>
2023-05-16 12:36:17 -0400

    adds pinecone vectorstore example

--- src/stories/5-indexes/indexes.stories.ts
+++ src/stories/5-indexes/indexes.stories.ts
@@ -1,37 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import fs from 'fs';
-import { z } from 'zod';
-import {
-  ChatPromptTemplate,
-  HumanMessagePromptTemplate,
-  PromptTemplate,
-  SystemMessagePromptTemplate,
-} from 'langchain/prompts';
-import TipTap from '../../components/TipTap/TipTap';
 import Chat from '../../components/Chat/Chat';
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { HumanChatMessage, SystemChatMessage } from 'langchain/schema';
-import { StructuredOutputParser } from 'langchain/output_parsers';
 import { OpenAI } from 'langchain/llms/openai';
-import { LLMChain, SimpleSequentialChain } from 'langchain/chains';
-import { BufferWindowMemory } from 'langchain/memory';
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { CharacterTextSplitter } from 'langchain/text_splitter';
+import { PineconeClient } from '@pinecone-database/pinecone';
+// We are importing one of the special chains that Langchain provides for us
+import { RetrievalQAChain } from 'langchain/chains';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
 
 //##########
 // LangChain
 //##########
 
-const chatModel = new ChatOpenAI({ temperature: 0, openAIApiKey: import.meta.env.VITE_OPENAI_API_KEY });
 const llm = new OpenAI({ openAIApiKey: import.meta.env.VITE_OPENAI_API_KEY });
 const splitter = new CharacterTextSplitter({
   separator: '\n',
   chunkSize: 150,
   chunkOverlap: 10,
 });
-const text = fs.readFileSync('assets/', 'utf8');
+const loader = new TextLoader('src/stories/assets/documents/vector_store_guide.txt');
 
 const memoryStoreExample = async () => {
   try {
@@ -56,9 +45,38 @@ const memoryStoreExample = async () => {
   // Load the docs into the vector store
 };
 
+// Retriever Docs
+const client = new PineconeClient();
+await client.init({
+  apiKey: import.meta.env.VITE_PINECONE_API_KEY,
+  environment: import.meta.env.VITE_PINECONE_ENVIRONMENT,
+});
+const pineconeIndex = client.Index(import.meta.env.VITE_PINECONE_INDEX);
+
+const vectorStore = await PineconeStore.fromExistingIndex(
+  new OpenAIEmbeddings({ openAIApiKey: import.meta.env.VITE_OPENAI_API_KEY }),
+  { pineconeIndex },
+);
+const queryPinecone = async (msg: string) => {
+  const result = await vectorStore.similaritySearch(msg, 1);
+  const chain = RetrievalQAChain.fromLLM(llm, vectorStore.asRetriever());
+  try {
+    const res = await chain.call({
+      query: msg,
+    });
+
+    // return res.text;
+    return result[0].pageContent;
+  } catch (error: any) {
+    console.error(error);
+    return error.message;
+  }
+};
+
 // #########
 // Storybook
 // #########
+
 const meta = {
   title: 'Indexes/Stories',
   component: Chat,
@@ -76,6 +94,13 @@ type Story = StoryObj<typeof meta>;
 export const MemoryStoreExample: Story = {
   args: {
     onSubmitHandler: memoryStoreExample,
+    startingValue: ``,
+  },
+};
+
+export const PineconeExample: Story = {
+  args: {
+    onSubmitHandler: queryPinecone,
     startingValue: ``,
   },
 };
