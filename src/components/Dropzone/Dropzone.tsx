59f61c8c4f4c0145d216acd5d18a76c423d89625
Josh Mabry <mabry1985@gmail.com>
2023-06-24 10:25:43 -0700

    Add Dropzone component for file uploads

---
+++ src/components/Dropzone/Dropzone.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useCallback, ReactNode } from 'react';
+
+interface DropzoneProps {
+  children: ReactNode;
+  onFileDrop: (file: File) => void;
+}
+
+const Dropzone: React.FC<DropzoneProps> = ({ children, onFileDrop }) => {
+  const [highlight, setHighlight] = useState(false);
+
+  const handleDrop = useCallback(
+    (event: React.DragEvent<HTMLDivElement>) => {
+      event.preventDefault();
+      const files = event.dataTransfer.files;
+      if (files && files.length > 0) {
+        const droppedFile = files[0];
+        onFileDrop(droppedFile);
+      }
+      setHighlight(false);
+    },
+    [onFileDrop],
+  );
+
+  const handleDragOver = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setHighlight(true);
+  }, []);
+
+  const handleDragLeave = useCallback(() => {
+    setHighlight(false);
+  }, []);
+
+  return (
+    <div
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      style={{ border: highlight ? '2px dashed #000' : '2px solid #000' }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Dropzone;
