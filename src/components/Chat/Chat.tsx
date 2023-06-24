cae4dc2cda84c6a3ceb7f31188d9e663afa54192
Josh Mabry <mabry1985@gmail.com>
2023-06-24 08:42:44 -0700

    Add useKeyPress to Chat component

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef, useCallback, useId } from 'react';
 import { ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { useClickAway } from '@uidotdev/usehooks';
+import { useClickAway, useKeyPress } from '@uidotdev/usehooks';
 import ReactMarkdown from 'react-markdown';
 import './Chat.css';
 // https://chatscope.io/storybook/react/?path=/story/documentation-introduction--page
@@ -20,6 +20,12 @@ const Chat: React.FC<ChatProps> = ({ onSubmitHandler, height, startingValue, nam
   const [messages, setMessages] = useState<MessageModel[]>([]);
   const [loading, setLoading] = useState(false);
   const [visible, setVisible] = useState(false);
+  const onKeyPress = (e: KeyboardEvent) => {
+    if (!visible) return;
+    e.preventDefault();
+    setVisible(false);
+  };
+  useKeyPress('Escape', onKeyPress);
   const ref = useClickAway(() => {
     setVisible(false);
   });
