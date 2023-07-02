903411f36255b9a81fac859ac599ec01e8fc10b3
Josh Mabry <mabry1985@gmail.com>
2023-07-01 17:42:12 -0700

    Remove unnecessary gradient and transition in AudioWave component

--- src/components/AudioWave/AudioWave.tsx
+++ src/components/AudioWave/AudioWave.tsx
@@ -30,12 +30,6 @@ const AudioWaveform: React.FC<AudioWaveformProps> = ({ isOn, audioContext }) => 
           .angle((d, i) => i * ((2 * Math.PI) / bufferLength))
           .radius((d) => (1 - d / 255) * radius);
 
-        const gradient = svg.append('defs').append('radialGradient').attr('id', 'radial-gradient');
-
-        gradient.append('stop').attr('offset', '0%').attr('stop-color', '#9370DB');
-
-        gradient.append('stop').attr('offset', '100%').attr('stop-color', 'white');
-
         function renderFrame() {
           requestAnimationFrame(renderFrame);
 
@@ -45,15 +39,11 @@ const AudioWaveform: React.FC<AudioWaveformProps> = ({ isOn, audioContext }) => 
             .selectAll('path')
             .data([dataArray])
             .join('path')
-            .transition() // Add transition
-            .duration(100) // Adjust duration to match data update rate
-            .ease(d3.easeLinear) // Use linear easing for smooth transition
             .attr('transform', `translate(${width / 2}, ${height / 2})`)
             .attr('d', radialLine)
             .attr('stroke', 'white')
-            .attr('stroke-width', 2)
-            .attr('fill', 'url(#radial-gradient)')
-            .attr('opacity', 0.5);
+            .attr('stroke-width', 5)
+            .attr('fill', 'none');
         }
 
         renderFrame();
@@ -78,9 +68,8 @@ const AudioWaveform: React.FC<AudioWaveformProps> = ({ isOn, audioContext }) => 
 
   return (
     <div
-      className=" w-[150px] h-[150px] transition-opacity duration-500"
+      className="fixed w-[150px] h-[150px] transition-opacity duration-500"
       style={{
-        position: 'fixed',
         bottom: '0',
         left: '0',
         zIndex: 100,
