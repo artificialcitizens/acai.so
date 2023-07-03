61d4ada7812d7e47c0a2de8f8fe1f397dfbfa980
Josh Mabry <mabry1985@gmail.com>
2023-07-02 20:49:14 -0700

    Add new sidebar components

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,24 @@ import Whisper from './components/Whisper';
 import ElevenLabs from './components/Elevenlabs/ElevenLabs';
 import SpeechRecognition from './components/SpeechRecognition/SpeechRecognition';
 import { recognitionRouter } from './components/SpeechRecognition/recognition-manager';
-import ToastManager, { toastifyAgentThought, toastifyInfo } from './components/Toast';
+import ToastManager, { toastifyAgentObservation, toastifyAgentThought, toastifyInfo } from './components/Toast';
 import SocketContext from './context/SocketContext';
 import AudioWaveform from './components/AudioWave/AudioWave';
 import SBSidebar from './components/Sidebar';
 import { Header } from './components/Header/Header';
 import TabManager from './components/Tabs';
 import StorageMeter from './components/StorageMeter/StorageMeter';
+import { ExpansionPanel } from '@chatscope/chat-ui-kit-react';
+import NotificationCenter from './components/NotificationCenter';
+import Chat from './components/Chat/Chat';
+import { avaChat } from './components/Chat/chat-routes';
+import SBSearch from './components/Search';
+import ScratchPad from './components/ScratchPad/ScratchPad';
 import { useTabs } from './hooks/use-tabs';
 
-export type State = 'ava' | 'notes';
+export type State = 'idle' | 'passive' | 'ava' | 'notes';
 
+// const [userLocation, setUserLocation] = useState<string>('Portland, OR');
 // const getGeolocation = () => {
 //   if ('geolocation' in navigator) {
 //     navigator.geolocation.getCurrentPosition(
@@ -34,12 +41,20 @@ function App() {
   const [transcript, setTranscript] = useState<string>('');
   // const [voice2voice, setVoice2voice] = useState<boolean>(false);
   const [speechRecognition, setSpeechRecognition] = useState<boolean>(true);
-  const [currentState, setCurrentState] = useState<string>('ava');
-  const [avaListening, setAvaListening] = useState<boolean>(false);
+  const [currentState, setCurrentState] = useState<string>('passive');
   const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
-  const [userLocation, setUserLocation] = useState<string>('Portland, OR');
   const { tabs, activeTab, createTab, deleteTab, updateContent, setActiveTab } = useTabs();
+  const [currentTool, setCurrentTool] = useState<string>('');
+  const [chatOpen, setChatOpen] = useState(true);
+  const [agentThoughtsOpen, setAgentThoughtsOpen] = useState(true);
 
+  const toggleChat = () => {
+    setChatOpen(!chatOpen);
+  };
+
+  const toggleAgentThoughts = () => {
+    setAgentThoughtsOpen(!agentThoughtsOpen);
+  };
   // useEffect(() => {
   //   getGeolocation();
   // }, []);
@@ -56,8 +71,7 @@ function App() {
     const handleConnect = () => console.log(`Connected: ${socket.id}`);
     const handleMessage = (message: string) => console.log(message);
     const handleDisconnect = () => console.log(`Disconnected: ${socket.id}`);
-    const handleCreateTab = async (args) => {
-      console.log('creating-tab from socket');
+    const handleCreateTab = async (args: { title: string; content: string }) => {
       const id = Date.now().toString();
       updateContent(id, {
         title: args.title,
@@ -102,10 +116,9 @@ function App() {
       activateAudioContext();
     }
   };
-  // Example usage:
   return (
     <div onClick={handleWindowClick}>
-      <AudioWaveform isOn={avaListening} audioContext={audioContext} />
+      <AudioWaveform isOn={currentState === 'ava'} audioContext={audioContext} />
       <ToastManager />
       <div className="flex flex-col min-h-screen">
         <Header />
@@ -119,41 +132,60 @@ function App() {
             setActiveTab={setActiveTab}
           />
           <SBSidebar>
-            <div>
-              <SpeechRecognition
-                active={speechRecognition}
-                onClick={() => {
-                  setSpeechRecognition(!speechRecognition);
-                }}
-                onTranscriptionComplete={async (t) => {
-                  console.log('speech', t);
-                  if ((t === 'Ava' || t === 'ava') && !avaListening) {
-                    setAvaListening(true);
-                  } else if (t.toLowerCase() === 'cancel' && avaListening) {
-                    setAvaListening(false);
-                    return;
-                  }
+            {' '}
+            <ExpansionPanel title="Settings">
+              {' '}
+              <div>
+                <SpeechRecognition
+                  active={speechRecognition}
+                  onClick={() => {
+                    setSpeechRecognition(!speechRecognition);
+                  }}
+                  onTranscriptionComplete={async (t) => {
+                    console.log('speech', t);
+                    if ((t === 'Ava' || t === 'ava') && currentState !== 'ava') {
+                      setCurrentState('ava');
+                    } else if (t.toLowerCase() === 'cancel') {
+                      setCurrentState('passive');
+                      return;
+                    } else if (t.toLowerCase() === 'take notes') {
+                      setCurrentState('notes');
+                      toastifyInfo('Taking notes');
+                    }
+                    if (t.split(' ').length < 3 || currentState === 'passive') return;
 
-                  if (t.split(' ').length < 3 || !avaListening) return;
+                    const response = await recognitionRouter({ state: currentState, transcript: t });
 
-                  if (t.toLowerCase() === 'take notes' && avaListening) {
-                    setCurrentState('notes');
-                    toastifyInfo('Taking notes');
-                  }
-                  const response = await recognitionRouter({ state: currentState, transcript: t });
-                  console.log(response);
-                  setTranscript(response as string);
-                }}
-              />
-              <ElevenLabs text={transcript} voice="ava" />
-              <Whisper
+                    console.log(response);
+
+                    setTranscript(response as string);
+                  }}
+                />
+                <ElevenLabs text={transcript} voice="ava" />
+                {/* <Whisper
                 onRecordingComplete={(blob) => console.log(blob)}
                 onTranscriptionComplete={async (t) => {
                   console.log('Whisper Server Response', t);
                 }}
-              />
-              <StorageMeter />
-            </div>
+              /> */}
+                <StorageMeter />
+              </div>
+            </ExpansionPanel>
+            <ExpansionPanel title="Search">
+              <SBSearch />
+            </ExpansionPanel>
+            <ExpansionPanel title="Notes">
+              <ScratchPad id="Notes" />
+            </ExpansionPanel>
+            <ExpansionPanel title="Observations">
+              <NotificationCenter placeholder="Nothing to see here 👀" secondaryFilter="agent-observations" />
+            </ExpansionPanel>
+            <ExpansionPanel title="Agent" isOpened={agentThoughtsOpen} onChange={toggleAgentThoughts}>
+              <NotificationCenter placeholder="A place for AI to ponder" secondaryFilter="agent-thought" />
+            </ExpansionPanel>
+            <ExpansionPanel className="flex-grow" title="Chat" isOpened={chatOpen} onChange={toggleChat}>
+              <Chat name="Ava" avatar=".." onSubmitHandler={async (message) => avaChat(message)} />
+            </ExpansionPanel>
           </SBSidebar>
         </main>
       </div>
