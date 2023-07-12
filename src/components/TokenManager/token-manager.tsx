ea7e69ef8bb6b0c8cefa2b4adb0f8cf106ed13f1
Josh Mabry <mabry1985@gmail.com>
2023-07-12 00:28:58 -0700

    add token manager component

---
+++ src/components/TokenManager/token-manager.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, FormEvent, useState } from 'react';
+import useCookieStorage from '../../hooks/use-cookie-storage';
+
+const TokenManager: React.FC = () => {
+  const [openAIKey, setOpenAIKey] = useCookieStorage('OPENAI_KEY');
+  const [formKey, setFormKey] = useState<string>('');
+
+  useEffect(() => {
+    if (openAIKey) {
+      setFormKey(openAIKey);
+    }
+  }, [openAIKey]);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    setOpenAIKey(formKey, 30);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        OpenAI API Key:
+        <input type="password" value={formKey} onChange={(e) => setFormKey(e.target.value)} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
+
+export default TokenManager;
