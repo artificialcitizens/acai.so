cd089ea3e9712852587f66b6b9205718ab9c21c0
Josh Mabry <mabry1985@gmail.com>
2023-07-23 23:18:19 -0700

    removes storybook

--- .storybook/manager.js
+++
@@ -1,9 +0,0 @@
-import { addons } from '@storybook/manager-api';
-import customTheme from './customTheme';
-
-addons.setConfig({
-  theme: customTheme,
-  sidebar: {
-    collapsedRoots: ['models', 'prompts', 'chains', 'agents', 'memory', 'documents', 'indexes', 'components'],
-  },
-});
