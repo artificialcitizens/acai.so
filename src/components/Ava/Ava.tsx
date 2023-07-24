56dc8459fcdad2377de40c7a0b735357961fa038
Josh Mabry <mabry1985@gmail.com>
2023-07-23 22:04:31 -0700

    add ava component and state machine

---
+++ src/components/Ava/Ava.tsx
@@ -0,0 +1,101 @@
+import React, { useContext } from 'react';
+import SBSidebar from '../../components/Sidebar';
+import { ProjectLinks } from '../../components/ProjectLinks/ProjectLinks';
+import StorageMeter from '../../components/StorageMeter/StorageMeter';
+import { ExpansionPanel } from '@chatscope/chat-ui-kit-react';
+import NotificationCenter from '../../components/NotificationCenter';
+import Chat from '../../components/Chat/Chat';
+import SBSearch from '../../components/Search';
+import ScratchPad from '../../components/ScratchPad/ScratchPad';
+import TokenManager from '../../components/TokenManager/token-manager';
+import { useSelector } from '@xstate/react';
+import useCookieStorage from '../../hooks/use-cookie-storage';
+import { useAva } from '../../hooks/use-ava';
+import { GlobalStateContext, GlobalStateContextValue } from '../../context/GlobalStateContext';
+import { makeObservations, queryPinecone } from '../../endpoints';
+import { useLocation } from 'react-router-dom';
+import { FloatingButton } from '../FloatingButton/FloatingButton';
+
+export const Ava = () => {
+  const { appStateService, uiStateService, agentStateService }: GlobalStateContextValue =
+    useContext(GlobalStateContext);
+  const location = useLocation();
+  const workspaceId = location.pathname.split('/')[1];
+  const systemNotes = useSelector(agentStateService, (state) => state.context[workspaceId]?.systemNotes) || '';
+  const [openAIApiKey] = useCookieStorage('OPENAI_KEY');
+  // const { vectorstore, addDocuments, similaritySearchWithScore } = useMemoryVectorStore(
+  //   '',
+  //   // add only tabs that are set to be included in the context of the language model
+  //   // @TODO: add a tool for Ava to see what the user is working on
+  //   // workspace ? workspace.data.tiptap.tabs.map((tab) => tab.isContext && tab.content).join('\n') : '',
+  // );
+  const [fetchResponse, avaLoading] = useAva();
+
+  const toggleChat = () => {
+    uiStateService.send({ type: 'TOGGLE_AGENT_CHAT' });
+  };
+
+  const toggleAgentThoughts = () => {
+    uiStateService.send({ type: 'TOGGLE_AGENT_THOUGHTS' });
+  };
+
+  return (
+    <SBSidebar>
+      {' '}
+      <ExpansionPanel data-ava-element="junk-drawer-panel-toggle" title="Junk Drawer">
+        <div className="w-full">
+          <ProjectLinks />
+          <SBSearch
+            onSubmit={async (val) => {
+              const response = await queryPinecone(val);
+              const newTab = {
+                id: Date.now().toString(),
+                title: val,
+                content: response,
+                workspaceId,
+              };
+              appStateService.send({ type: 'ADD_TAB', tab: newTab });
+            }}
+          />
+          <TokenManager />
+          <StorageMeter />
+        </div>
+      </ExpansionPanel>
+      <ExpansionPanel title="Agent Settings">
+        <ScratchPad
+          placeholder="Agent Refinement"
+          content={systemNotes}
+          handleInputChange={(e) => {
+            agentStateService.send({
+              type: 'UPDATE_SYSTEM_NOTES',
+              workspaceId: workspaceId,
+              systemNotes: e.target.value,
+            });
+          }}
+        />
+      </ExpansionPanel>
+      <ExpansionPanel
+        title="Agent"
+        data-ava-element="TOGGLE_AGENT_THOUGHTS"
+        isOpened={uiStateService.getSnapshot().context.thoughtsOpen}
+        onChange={toggleAgentThoughts}
+        onClick={toggleAgentThoughts}
+      >
+        <NotificationCenter placeholder="A place for AI to ponder 🤔" secondaryFilter="agent-thought" />
+      </ExpansionPanel>
+      {openAIApiKey && (
+        <Chat
+          name="Ava"
+          avatar=".."
+          onSubmitHandler={async (message) => {
+            const systemMessage = systemNotes;
+            const response = await fetchResponse(message, systemMessage);
+            return response;
+          }}
+        />
+      )}
+    </SBSidebar>
+  );
+};
+
+export default Ava;
---
+++ src/state/agent.xstate.ts
@@ -0,0 +1,227 @@
+import { createMachine, assign } from 'xstate';
+
+export interface ChatHistory {
+  id: string;
+  text: string;
+  timestamp: string;
+  type: 'user' | 'ava';
+}
+
+type AgentContext = {
+  id: string;
+  loading: boolean;
+  workspaceId: string;
+  systemNotes: string;
+  recentChatHistory: ChatHistory[];
+  agentLogs: {
+    [key: string]: any;
+  };
+  agentTools: {
+    [key: string]: {
+      enabled: boolean;
+      settings: object;
+    };
+  };
+};
+
+type AgentWorkspace = {
+  [key: string]: AgentContext;
+};
+
+type AgentEvent =
+  | { type: 'LOAD'; workspaceId: string }
+  | { type: 'UPDATE'; agent: Partial<AgentContext> }
+  | { type: 'TOGGLE_TOOL'; toolName: string }
+  | { type: 'UPDATE_SYSTEM_NOTES'; workspaceId: string; systemNotes: string }
+  | { type: 'UPDATE_CHAT_HISTORY'; workspaceId: string; recentChatHistory: ChatHistory[] }
+  | { type: 'CREATE_AGENT'; workspaceId: string };
+
+/**
+ * Save AgentWorkspace to local storage
+ */
+const saveAgentState = (state: AgentWorkspace) => {
+  localStorage.setItem('agentState', JSON.stringify(state));
+};
+
+/**
+ * Load AgentWorkspace from local storage
+ */
+const loadAgentState = (): AgentWorkspace => {
+  const savedState = localStorage.getItem('agentState');
+  if (savedState) {
+    return JSON.parse(savedState);
+  } else {
+    const initialState: AgentWorkspace = {
+      UUIDxyz: {
+        id: 'UUIDxyzzd',
+        loading: false,
+        workspaceId: 'UUIDxyz',
+        systemNotes: '',
+        recentChatHistory: [
+          {
+            id: 'UUIDxyz',
+            text: 'Hello, how can I help you?',
+            timestamp: '1621090800000',
+            type: 'ava',
+          },
+          {
+            id: 'UUIDxyz',
+            text: 'I need help with my account',
+            timestamp: '1651090800000',
+            type: 'user',
+          },
+        ],
+        agentLogs: {},
+        agentTools: {},
+      },
+    }; // Define your initial state here
+    saveAgentState(initialState);
+    return initialState;
+  }
+};
+
+export const createAgent = (workspaceId: string): AgentContext => {
+  return {
+    id: workspaceId,
+    loading: false,
+    workspaceId: workspaceId,
+    systemNotes: '',
+    recentChatHistory: [],
+    agentLogs: {},
+    agentTools: {},
+  };
+};
+
+// Define the initial context
+const initialAgentContext: AgentWorkspace = loadAgentState();
+
+// Define the machine
+export const agentMachine = createMachine<AgentWorkspace, AgentEvent>({
+  id: 'agent',
+  initial: 'idle',
+  context: initialAgentContext,
+  states: {
+    idle: {
+      on: {
+        LOAD: {
+          target: 'loading',
+          actions: assign((context, event) => {
+            if (event.workspaceId) {
+              const updatedContext = {
+                ...context,
+                [event.workspaceId]: {
+                  ...context[event.workspaceId],
+                  loading: true,
+                },
+              };
+              saveAgentState(updatedContext);
+              return updatedContext;
+            }
+            return context;
+          }),
+        },
+      },
+    },
+    loading: {
+      on: {
+        UPDATE: {
+          target: 'idle',
+          actions: assign((context, event) => {
+            if (event.agent.workspaceId) {
+              const updatedContext = {
+                ...context,
+                [event.agent.workspaceId]: {
+                  ...context[event.agent.workspaceId],
+                  ...event.agent,
+                  loading: false,
+                },
+              };
+              saveAgentState(updatedContext);
+              return updatedContext;
+            }
+            return context;
+          }),
+        },
+      },
+    },
+    error: {
+      on: {
+        LOAD: {
+          target: 'loading',
+          actions: assign((context, event) => {
+            if (event.workspaceId) {
+              const updatedContext = {
+                ...context,
+                [event.workspaceId]: {
+                  ...context[event.workspaceId],
+                  loading: true,
+                },
+              };
+              saveAgentState(updatedContext);
+              return updatedContext;
+            }
+            return context;
+          }),
+        },
+      },
+    },
+  },
+  on: {
+    UPDATE_SYSTEM_NOTES: {
+      actions: assign((context, event) => {
+        if (event.workspaceId && context[event.workspaceId]) {
+          const updatedContext = {
+            ...context,
+            [event.workspaceId]: {
+              ...context[event.workspaceId],
+              systemNotes: event.systemNotes,
+            },
+          };
+          saveAgentState(updatedContext);
+          return updatedContext;
+        }
+        return context;
+      }),
+    },
+    UPDATE_CHAT_HISTORY: {
+      actions: assign((context, event) => {
+        if (event.workspaceId && context[event.workspaceId]) {
+          const updatedContext = {
+            ...context,
+            [event.workspaceId]: {
+              ...context[event.workspaceId],
+              recentChatHistory: event.recentChatHistory,
+            },
+          };
+          saveAgentState(updatedContext);
+          return updatedContext;
+        }
+        return context;
+      }),
+    },
+    TOGGLE_TOOL: {
+      actions: assign((context, event) => {
+        const workspaceIds = Object.keys(context);
+        for (const workspaceId of workspaceIds) {
+          const tool = context[workspaceId].agentTools[event.toolName];
+          if (tool) {
+            tool.enabled = !tool.enabled;
+          }
+        }
+        saveAgentState(context);
+        return context;
+      }),
+    },
+    CREATE_AGENT: {
+      actions: assign((context, event) => {
+        const newAgent = createAgent(event.workspaceId);
+        const updatedContext = {
+          ...context,
+          [event.workspaceId]: newAgent,
+        };
+        saveAgentState(updatedContext);
+        return updatedContext;
+      }),
+    },
+  },
+});
