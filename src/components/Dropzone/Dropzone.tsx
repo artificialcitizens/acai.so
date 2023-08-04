ee69e9879de99bd186df70b0d54e38c883674cc4
Josh Mabry <mabry1985@gmail.com>
2023-08-03 22:40:09 -0700

    updates to manage a dir or file

--- src/components/Dropzone/Dropzone.tsx
+++ src/components/Dropzone/Dropzone.tsx
@@ -1,24 +1,71 @@
 import React, { useState, useCallback, ReactNode } from 'react';
+import { slugify } from '../../utils/data-utils';
 
 interface DropzoneProps {
   children: ReactNode;
-  onFileDrop: (file: File) => void;
+  onFilesDrop: (files: File[], name: string) => void;
 }
 
-const Dropzone: React.FC<DropzoneProps> = ({ children, onFileDrop }) => {
+const Dropzone: React.FC<DropzoneProps> = ({ children, onFilesDrop }) => {
   const [highlight, setHighlight] = useState(false);
 
   const handleDrop = useCallback(
-    (event: React.DragEvent<HTMLDivElement>) => {
+    async (event: React.DragEvent<HTMLDivElement>) => {
       event.preventDefault();
-      const files = event.dataTransfer.files;
-      if (files && files.length > 0) {
-        const droppedFile = files[0];
-        onFileDrop(droppedFile);
+      const items = event.dataTransfer.items;
+      const droppedFiles: File[] = [];
+
+      const processEntry = async (entry: any): Promise<File[] | File> => {
+        if (entry.isFile) {
+          return new Promise<File>((resolve) => {
+            entry.file((file) => resolve(file));
+          });
+        } else if (entry.isDirectory) {
+          return readDirectory(entry.createReader());
+        }
+      };
+
+      const readDirectory = async (reader: any): Promise<File[]> => {
+        const entries = await readEntries(reader);
+        const folderFiles = [];
+
+        for (const entry of entries) {
+          const fileOrFiles = await processEntry(entry);
+          if (Array.isArray(fileOrFiles)) {
+            folderFiles.push(...fileOrFiles);
+          } else {
+            folderFiles.push(fileOrFiles);
+          }
+        }
+
+        return folderFiles;
+      };
+
+      const readEntries = (reader: any): Promise<any[]> => {
+        return new Promise<any[]>((resolve) => {
+          reader.readEntries((entries) => resolve(entries));
+        });
+      };
+
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        if (item.kind === 'file') {
+          const fileOrFiles = await processEntry(item.webkitGetAsEntry());
+          if (Array.isArray(fileOrFiles)) {
+            droppedFiles.push(...fileOrFiles);
+          } else {
+            droppedFiles.push(fileOrFiles);
+          }
+        }
+      }
+
+      if (droppedFiles.length > 0) {
+        const name = slugify(droppedFiles[0].name.split('.')[0]);
+        onFilesDrop(droppedFiles, name);
       }
       setHighlight(false);
     },
-    [onFileDrop],
+    [onFilesDrop],
   );
 
   const handleDragOver = useCallback((event: React.DragEvent<HTMLDivElement>) => {
