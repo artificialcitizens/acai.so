fa03a1551207a105aea8895d08cd6fcd62677a89
Josh Mabry <mabry1985@gmail.com>
2023-08-04 00:14:59 -0700

    create use bark hook

---
+++ src/hooks/use-bark.ts
@@ -0,0 +1,24 @@
+// hooks/useBark.ts
+import { useCallback } from 'react';
+import axios from 'axios';
+
+export const useBark = () => {
+  const synthesizeBarkSpeech = useCallback(async (inputText: string) => {
+    const options = {
+      method: 'POST',
+      url: `http://192.168.4.44:5000/bark-inference`,
+      headers: {
+        'content-type': 'application/json',
+      },
+      data: {
+        text: inputText,
+      },
+      responseType: 'arraybuffer' as 'json',
+    };
+
+    const speechDetails = await axios.request(options);
+    return speechDetails.data;
+  }, []);
+
+  return synthesizeBarkSpeech;
+};
