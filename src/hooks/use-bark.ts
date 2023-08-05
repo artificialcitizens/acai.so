1b82aa4f50223350b7a8be4fc3dbd52243a86b1b
Josh Mabry <mabry1985@gmail.com>
2023-08-05 13:27:05 -0700

    update bark hook to take voice arg

--- src/hooks/use-bark.ts
+++ src/hooks/use-bark.ts
@@ -3,22 +3,26 @@ import { useCallback } from 'react';
 import axios from 'axios';
 
 export const useBark = () => {
-  const synthesizeBarkSpeech = useCallback(async (inputText: string) => {
-    const options = {
-      method: 'POST',
-      url: `http://192.168.4.44:5000/bark-inference`,
-      headers: {
-        'content-type': 'application/json',
-      },
-      data: {
-        text: inputText,
-      },
-      responseType: 'arraybuffer' as 'json',
-    };
+  const synthesizeBarkSpeech = useCallback(
+    async ({ inputText, voicePreset }: { inputText: string; voicePreset: string }) => {
+      const options = {
+        method: 'POST',
+        url: `http://192.168.4.44:5000/bark-inference`,
+        headers: {
+          'content-type': 'application/json',
+        },
+        data: {
+          text: inputText,
+          voice: voicePreset,
+        },
+        responseType: 'arraybuffer' as 'json',
+      };
 
-    const speechDetails = await axios.request(options);
-    return speechDetails.data;
-  }, []);
+      const speechDetails = await axios.request(options);
+      return speechDetails.data;
+    },
+    [],
+  );
 
   return synthesizeBarkSpeech;
 };
