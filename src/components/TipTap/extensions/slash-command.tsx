0b7584e34f9f1e1c71b7187fb150db0cca3ff7bb
Josh Mabry <mabry1985@gmail.com>
2023-08-25 02:15:28 -0700

    update slash command key nav

--- src/components/TipTap/extensions/slash-command.tsx
+++ src/components/TipTap/extensions/slash-command.tsx
@@ -247,11 +247,15 @@ const CommandList = ({
   command,
   editor,
   range,
+  hidePopup,
+  isOpen,
 }: {
   items: CommandItemProps[];
   command: any;
   editor: any;
   range: any;
+  hidePopup: () => void;
+  isOpen: boolean;
 }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
@@ -295,34 +299,63 @@ const CommandList = ({
   );
 
   useEffect(() => {
-    const navigationKeys = ['ArrowUp', 'ArrowDown', 'Enter'];
     const onKeyDown = (e: KeyboardEvent) => {
-      if (navigationKeys.includes(e.key)) {
-        e.preventDefault();
-        if (e.key === 'ArrowUp') {
-          setSelectedIndex((selectedIndex + items.length - 1) % items.length);
+      const navigationKeys = ['ArrowUp', 'ArrowDown', 'Enter'];
+      const closeKeys = ['ArrowLeft', 'ArrowRight', 'Backspace', 'Escape'];
+      if (
+        isOpen &&
+        (navigationKeys.includes(e.key) || closeKeys.includes(e.key))
+      ) {
+        if (navigationKeys.includes(e.key)) {
+          e.preventDefault();
+          if (e.key === 'ArrowUp') {
+            setSelectedIndex((selectedIndex + items.length - 1) % items.length);
+            return true;
+          }
+          if (e.key === 'ArrowDown') {
+            setSelectedIndex((selectedIndex + 1) % items.length);
+            return true;
+          }
+          if (e.key === 'Enter') {
+            selectItem(selectedIndex);
+            return true;
+          }
+        }
+        if (closeKeys.includes(e.key)) {
+          // Save current selection
+          const { from, to } = editor.state.selection;
+          hidePopup();
+          // Restore selection
+          editor.chain().focus().setTextSelection({ from, to }).run();
           return true;
         }
-        if (e.key === 'ArrowDown') {
-          setSelectedIndex((selectedIndex + 1) % items.length);
-          return true;
-        }
-        if (e.key === 'Enter') {
-          selectItem(selectedIndex);
-          return true;
-        }
-        return false;
       }
+      return false;
     };
+
     document.addEventListener('keydown', onKeyDown);
     return () => {
       document.removeEventListener('keydown', onKeyDown);
     };
-  }, [items, selectedIndex, setSelectedIndex, selectItem]);
+  }, [
+    items,
+    selectedIndex,
+    setSelectedIndex,
+    selectItem,
+    hidePopup,
+    isOpen,
+    editor,
+  ]);
 
   useEffect(() => {
     setSelectedIndex(0);
   }, [items]);
+
+  useEffect(() => {
+    const buttons = document.querySelectorAll('#slash-command button');
+    const selectedButton = buttons[selectedIndex] as HTMLElement;
+    selectedButton?.focus();
+  }, [selectedIndex]);
 
   const commandListContainer = useRef<HTMLDivElement>(null);
 
@@ -370,11 +403,20 @@ const CommandList = ({
 const renderItems = () => {
   let component: ReactRenderer | null = null;
   let popup: any | null = null;
+  let isOpen = false;
 
   return {
     onStart: (props: { editor: Editor; clientRect: DOMRect }) => {
+      isOpen = true;
       component = new ReactRenderer(CommandList, {
-        props,
+        props: {
+          ...props,
+          hidePopup: () => {
+            popup?.[0].hide();
+            isOpen = false;
+          },
+          isOpen,
+        },
         editor: props.editor,
       });
 
@@ -387,6 +429,10 @@ const renderItems = () => {
         interactive: true,
         trigger: 'manual',
         placement: 'bottom-start',
+        onShown: (instance) => {
+          const firstItem = instance.popper.querySelector('button');
+          firstItem && firstItem.focus();
+        },
       });
     },
     onUpdate: (props: { editor: Editor; clientRect: DOMRect }) => {
