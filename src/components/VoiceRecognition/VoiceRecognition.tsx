05dfeb1cf8d990a94c10c937a3ba6b1fc1563fb5
Josh Mabry <mabry1985@gmail.com>
2023-09-05 21:37:45 -0700

    update button styles

--- src/components/VoiceRecognition/VoiceRecognition.tsx
+++ src/components/VoiceRecognition/VoiceRecognition.tsx
@@ -356,7 +356,12 @@ const VoiceRecognition: React.FC<VoiceRecognitionProps> = ({
             }
           }}
         />
-        <button type="submit">Submit</button>
+        <button
+          className="bg-neutral-900 text-acai-white px-4 py-2 rounded-md transition-colors duration-200 ease-in-out hover:bg-light focus:outline-none focus:ring-2 focus:ring-gray-50 focus:ring-opacity-50 cursor-pointer"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
