614d82528bdea1f806223870028170aaf2a7c5d3
Josh Mabry <mabry1985@gmail.com>
2023-09-23 13:08:47 -0700

    update to take arg

--- src/components/Proto/generate-file.js
+++ src/components/Proto/generate-file.js
@@ -2,15 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// type FileSystemNode = {
-//   [name: string]: {
-//     directory?: FileSystemNode;
-//     file?: {
-//       contents: string;
-//     };
-//   };
-// };
-
 const generateFileSystemTree = (directoryPath) => {
   const entries = fs.readdirSync(directoryPath, { withFileTypes: true });
 
@@ -38,13 +29,21 @@ const generateFileSystemTree = (directoryPath) => {
   }, {});
 };
 
-const files = generateFileSystemTree('src/components/Proto/vite'); // specify the path to the directory
+// Get the directory path from command line arguments
+// eslint-disable-next-line no-undef
+const directoryPath = process.argv[2] || 'src/components/Proto/apps/vite';
+
+const files = generateFileSystemTree(directoryPath);
 
 // Get the directory of the current module
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// Get the directory name from the directory path
+const directoryName = path.basename(directoryPath);
+
 // Write the file system tree to a new file in the current directory
 fs.writeFileSync(
-  path.join(dirname, 'vite/files.ts'),
+  // eslint-disable-next-line no-undef
+  path.join(dirname, directoryName + '-files.ts'),
   `export const files = ${JSON.stringify(files, null, 2)};`,
 );
