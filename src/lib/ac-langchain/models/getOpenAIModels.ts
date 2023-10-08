9e90e6c7dbb1225d7a49da4dd810660ebcd18cf2
Josh Mabry <mabry1985@gmail.com>
2023-10-08 13:44:59 -0700

    updates to remove instruct models

--- src/lib/ac-langchain/models/getOpenAIModels.ts
+++ src/lib/ac-langchain/models/getOpenAIModels.ts
@@ -3,7 +3,10 @@ import { getToken } from '../../../utils/config';
 
 const configuration = new Configuration({
   apiKey: getToken('OPENAI_KEY') || import.meta.env.VITE_OPENAI_KEY,
-  basePath: getToken('OPENAI_API_BASE') || (import.meta.env.VITE_OPENAI_API_BASE || 'https://api.openai.com/v1'),
+  basePath:
+    getToken('OPENAI_API_BASE') ||
+    import.meta.env.VITE_OPENAI_API_BASE ||
+    'https://api.openai.com/v1',
 });
 const openai = new OpenAIApi(configuration);
 
@@ -15,8 +18,8 @@ export const getOpenAIChatModels = async () => {
   const response = await openai.listModels();
   const models = response.data;
   // filter to only models containing gpt models
-  const filteredModels = models.data.filter((model) =>
-    model.id.includes('gpt'),
+  const filteredModels = models.data.filter(
+    (model) => model.id.includes('gpt') && !model.id.includes('instruct'),
   );
   // order by descending id
   filteredModels.sort((a, b) => {
