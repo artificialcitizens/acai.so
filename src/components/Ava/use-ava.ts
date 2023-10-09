5bc3ec0880589b6777c00e31751162ca0f47eb0c
Josh Mabry <mabry1985@gmail.com>
2023-10-09 14:47:49 -0700

    revert

--- src/components/Ava/use-ava.ts
+++ src/components/Ava/use-ava.ts
@@ -136,12 +136,12 @@ export const useAva = (): {
         const sysMessage = customPrompt
           ? await createCustomPrompt(customPrompt, '') //formattedChatHistory)
           : await createAvaChatPrompt(
-              userName || 'user',
+              userName || 'User',
               userLocation || 'Undisclosed',
               '',
               // formattedChatHistory,
             );
-        // console.log(currentAgent?.recentChatHistory)
+        console.log(currentAgent?.recentChatHistory);
         const response = await queryChat({
           systemMessage: sysMessage,
           message,
