1f79a4f326940bf9582a90bd1055100590e696a9
Josh Mabry <mabry1985@gmail.com>
2023-10-21 10:46:57 -0700

    update settings index

--- src/components/AvaNav/AvaNav.tsx
+++ src/components/AvaNav/AvaNav.tsx
@@ -55,7 +55,7 @@ export const AvaNav: React.FC<AvaSettingsProps> = ({
       </button>
       <button
         className="inline-block rounded-full mr-4 my-2.5 p-1 text-xs font-medium uppercase leading-tight text-white z-index-10 "
-        onMouseDown={() => openSettings(2)}
+        onMouseDown={() => openSettings(1)}
       >
         <span className="block [&>svg]:h-5 [&>svg]:w-5 md:[&>svg]:h-4 md:[&>svg]:w-4 [&>svg]:text-acai-white hover:pointer">
           <KnowledgeIcon />
