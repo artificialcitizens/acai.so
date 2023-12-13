bb9e070953cf4d083d0ba82929d636635b840596
Josh Mabry <mabry1985@gmail.com>
2023-12-12 16:43:34 -0800

    prevent early cutoff of stream tts

--- src/hooks/tts-hooks/use-xtts.ts
+++ src/hooks/tts-hooks/use-xtts.ts
@@ -68,21 +68,31 @@ export const useXtts = () => {
     }
   };
 
-  const handleUpload = () => {
+  const handleUpload = async () => {
     if (file) {
       const formData = new FormData();
       formData.append('wav_file', file);
 
-      fetch('http://192.168.4.186:8000/clone_speaker', {
-        method: 'POST',
-        body: formData,
-      });
+      const speakerResponse = await fetch(
+        'http://192.168.4.186:8000/clone_speaker',
+        {
+          method: 'POST',
+          body: formData,
+        },
+      );
+      toastifyInfo('Default speaker embedding loaded');
+      const speakerData = await speakerResponse.json();
+      speakerRef.current = speakerData;
+      localStorage.setItem(
+        'defaultSpeakerEmbedding',
+        JSON.stringify(speakerData),
+      );
     } else {
       console.error('No file selected');
     }
   };
 
-  const handleTTS = async (text: string, lang: any) => {
+  const handleTTS = async (text: string, lang: string) => {
     if (!speakerRef.current) return;
     setLoading(true);
 
@@ -94,104 +104,96 @@ export const useXtts = () => {
       return sample1 * (1 - fraction) + sample2 * fraction;
     }
 
-    await fetch('http://192.168.4.186:8000/tts_stream', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        text: text,
-        language: lang,
-        gpt_cond_latent: speakerRef.current.gpt_cond_latent,
-        speaker_embedding: speakerRef.current.speaker_embedding,
-        add_wav_header: false,
-      }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const audioContext = new window.AudioContext();
-        const playbackSpeed = 24000 / audioContext.sampleRate;
-        const scriptNode = audioContext.createScriptProcessor(4096, 1, 1);
-        scriptNode.connect(audioContext.destination);
-
-        const reader = response.body?.getReader();
-        if (!reader) {
-          throw new Error('Response body is null');
-        }
-        let audioQueue: any[] = [];
-        let isStreamingFinished = false;
-        let nextSample = 0;
-
-        scriptNode.onaudioprocess = (audioProcessingEvent) => {
-          const outputBuffer =
-            audioProcessingEvent.outputBuffer.getChannelData(0);
-          const fadeOutLength = Math.min(4000, outputBuffer.length); // adjust as needed
-          for (let i = 0; i < outputBuffer.length; i++) {
-            if (nextSample < audioQueue.length) {
-              const sampleIndex = Math.floor(nextSample);
-              const nextIndex = sampleIndex + 1;
-              const sampleFraction = nextSample - sampleIndex;
-              const interpolatedSample = linearInterpolate(
-                audioQueue[sampleIndex],
-                audioQueue[nextIndex],
-                sampleFraction,
-              );
-              outputBuffer[i] = interpolatedSample / 32768;
-              nextSample += playbackSpeed;
-            } else {
-              if (
-                isStreamingFinished &&
-                i >= outputBuffer.length - fadeOutLength
-              ) {
-                // Apply fade-out effect
-                const fadeOutFactor =
-                  1 -
-                  (i - (outputBuffer.length - fadeOutLength)) / fadeOutLength;
-                outputBuffer[i] *= fadeOutFactor;
-              } else {
-                outputBuffer[i] = 0; // Fill with silence if no data available
-              }
-              if (isStreamingFinished && i === outputBuffer.length - 1) {
-                scriptNode.disconnect();
-                audioContext.close();
-                setLoading(false);
-                break;
-              }
-            }
-          }
-        };
-
-        function processAudioChunk({
-          done,
-          value,
-        }: {
-          done: boolean;
-          value?: any;
-        }) {
-          if (!reader) return;
-          if (done) {
-            isStreamingFinished = true;
-            return;
-          }
-
-          // Convert the incoming data to Int16Array and add it to the queue
-          const rawData = new Int16Array(
-            value.buffer,
-            value.byteOffset,
-            value.byteLength / 2,
-          );
-          audioQueue = audioQueue.concat(Array.from(rawData));
-
-          reader.read().then(processAudioChunk);
-        }
-
-        reader.read().then(processAudioChunk);
-      })
-      .catch((error) => {
-        console.error('Error calling TTS service:', error);
+    try {
+      const response = await fetch('http://192.168.4.186:8000/tts_stream', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          text: text,
+          language: lang,
+          gpt_cond_latent: speakerRef.current.gpt_cond_latent,
+          speaker_embedding: speakerRef.current.speaker_embedding,
+          add_wav_header: false,
+        }),
       });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const audioContext = new window.AudioContext();
+      const playbackSpeed = 24000 / audioContext.sampleRate;
+      const scriptNode = audioContext.createScriptProcessor(4096, 1, 1);
+      scriptNode.connect(audioContext.destination);
+
+      const reader = response.body?.getReader();
+      if (!reader) {
+        throw new Error('Response body is null');
+      }
+
+      let audioQueue: number[] = [];
+      let isStreamingFinished = false;
+      let nextSample = 0;
+
+      scriptNode.onaudioprocess = (audioProcessingEvent) => {
+        const outputBuffer =
+          audioProcessingEvent.outputBuffer.getChannelData(0);
+        for (let i = 0; i < outputBuffer.length; i++) {
+          if (nextSample < audioQueue.length) {
+            const sampleIndex = Math.floor(nextSample);
+            const nextIndex = Math.min(sampleIndex + 1, audioQueue.length - 1);
+            const sampleFraction = nextSample - sampleIndex;
+            const interpolatedSample = linearInterpolate(
+              audioQueue[sampleIndex],
+              audioQueue[nextIndex],
+              sampleFraction,
+            );
+            outputBuffer[i] = interpolatedSample / 32768;
+            nextSample += playbackSpeed;
+          } else {
+            outputBuffer[i] = 0; // Fill with silence if no data available
+          }
+        }
+
+        if (isStreamingFinished && nextSample >= audioQueue.length) {
+          // Apply fade-out effect here if needed
+          scriptNode.disconnect();
+          audioContext.close();
+          setLoading(false);
+        }
+      };
+
+      const processAudioChunk = async ({
+        done,
+        value,
+      }: {
+        done: boolean;
+        value?: Uint8Array;
+      }) => {
+        if (done) {
+          isStreamingFinished = true;
+          return;
+        }
+
+        const rawData = new Int16Array(
+          value!.buffer,
+          value!.byteOffset,
+          value!.byteLength / 2,
+        );
+        audioQueue = audioQueue.concat(Array.from(rawData));
+
+        const nextChunk = await reader.read();
+        processAudioChunk(nextChunk);
+      };
+
+      const initialChunk = await reader.read();
+      processAudioChunk(initialChunk);
+    } catch (error) {
+      console.error('Error calling TTS service:', error);
+      setLoading(false);
+    }
   };
 
   return {
